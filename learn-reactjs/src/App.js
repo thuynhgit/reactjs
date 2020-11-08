d3cffed4594ca9297c94352275a652cfc7678a07
thuynhgit <thuynh240785@gmail.com>
2020-11-08 13:59:26 +0700

    todolist

--- learn-reactjs/src/App.js
+++ learn-reactjs/src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import TodoFeature from './features/Todo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          THUY NGUYEN
-        </p>
-      </header>
+      <TodoFeature />
     </div>
   );
 }
